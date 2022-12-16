04fd507cc2bd0f36e05711c3fc01ebaae3fc86c7
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2022-12-16 22:09:04 +0300

    add taskList

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { RiDeleteBack2Fill } from "react-icons/ri";
 
-const TaskList = ({task}) => {
+const TaskList = ({ task }) => {
   return (
-    <div>TaskList</div>
-  )
-}
+    <div>
+      {task.map((item) => {
+        const { id, task, date } = item;
+        return (
+          <div
+          key={id} 
+          className="mt-2 d-flex justify-content-between bg-secondary rounded-2 p-2">
+            <div>
+              <h4>{task}</h4>
+              <p>{date}</p>
+            </div>
 
-export default TaskList
+            <div>
+              <RiDeleteBack2Fill style={{}} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TaskList;
